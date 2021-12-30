f829dcfd6101f879b940cce28989858cfeefdb99
no-glue <nikolapav1985@gmail.com>
2021-12-30 16:37:21 +0100

    hth19 render test engine

--- scripts/render_test.py
+++ scripts/render_test.py
@@ -1,17 +1,31 @@
 from optparse import OptionParser
+import sys
+
+def do_render(frm,to,inputscript,out):
+    '''
+        do a render
+    '''
+    hou.hipFile.load(inputscript)
+    rnode = hou.node(out)
+    rnode.render(frame_range=(frm,to))
 
 def main():
     # run for main
     parser=OptionParser()
+    debug=True
 
     parser.add_option("-f","--from",dest="from",help="initial frame")
     parser.add_option("-t","--to",dest="to",help="last frame")
     parser.add_option("-i","--input",dest="inputscript",help="houdini script to use",metavar="FILE")
+    parser.add_option("-o","--output",dest="output",help="render engine to use")
 
     (options, args) = parser.parse_args()
 
-    print(options)
-    print(args)
+    debug and print(options)
+
+    do_render(options.from,options.to,options.input,options.output)
+
+    sys.exit(0)
 
 if __name__ == "__main__":
     # begin run script
